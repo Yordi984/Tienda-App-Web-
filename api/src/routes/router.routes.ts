246078e98601e17073c627195b4c816a0f44e49e
Yordi984 <135142211+Yordi984@users.noreply.github.com>
2025-05-26 13:27:56 -0500

    agrege los controladores crud para un producto

--- api/src/routes/router.routes.ts
+++ api/src/routes/router.routes.ts
@@ -6,13 +6,26 @@ import {
   iniciarSesionVendedor,
 } from "../controllers/vendedor.controller";
 
-const router = Router();
+import {
+  CrearProducto,
+  obtenerProductos,
+  editarProducto,
+  eliminarProducto,
+} from "../controllers/producto.controller";
 
+const router = Router();
+//comprador routes
 router.post("/comprador", CrearComprador);
 router.post("/loginComprador", iniciarSesion);
 
 //vendedor routes
 router.post("/vendedor", CrearVendedor);
 router.post("/loginVendedor", iniciarSesionVendedor);
+
+//producto routes
+router.post("/producto", CrearProducto);
+router.get("/productos", obtenerProductos);
+router.put("/producto/:id", editarProducto);
+router.delete("/producto/:id", eliminarProducto);
 
 export default router;
