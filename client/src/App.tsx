295ce17a3c8673b72c1a1735ceb84a4fcab3745e
Yordi984 <135142211+Yordi984@users.noreply.github.com>
2025-05-27 10:01:21 -0500

    instalacion de react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
 import Navbar from "./components/ui/Navbar";
-import HeaderComponent from "./components/ui/HeaderComponent";
-import ButtonComponent from "./components/ui/ButtonComponent";
-import SearchBar from "./components/ui/SearchBar";
-import InputComponent from "./components/ui/InputComponent";
-import ProductCard from "./components/ui/ProductCard";
+// import HeaderComponent from "./components/ui/HeaderComponent";
+// import ButtonComponent from "./components/ui/ButtonComponent";
+// import SearchBar from "./components/ui/SearchBar";
+// import InputComponent from "./components/ui/InputComponent";
+// import ProductCard from "./components/ui/ProductCard";
 
 function App() {
   return (
     <>
-      <HeaderComponent text="Hola ¿Qué compraras hoy?" />
+      {/* <HeaderComponent text="Hola ¿Qué compraras hoy?" />
       <ButtonComponent
         text="Comprar"
         color="green"
@@ -33,6 +33,8 @@ function App() {
         }
         onClick={() => console.log("Producto clickeado")}
       />
+      <Navbar /> */}
+
       <Navbar />
     </>
   );
