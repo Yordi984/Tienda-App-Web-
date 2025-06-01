92b6bb7351e924bb19c4acc1c506d38aaab79bb1
Yordi984 <135142211+Yordi984@users.noreply.github.com>
2025-05-31 22:01:42 -0500

    Revert "Merge pull request #7 from Yordi984/Yordi"
    This reverts commit 1d8a1c6da0b5e4652b221d46504493a79f207d7f, reversing
    changes made to 4c688ea012c8f17f0e20e4d0edb4965678586678.

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,28 +1,13 @@
-
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import session from "express-session";
-import passport from "./config/passport";
-import Router from "./routes/router.routes";
+import cors from 'cors';
+import express from 'express';
+import morgan from 'morgan';
+import router from './routes/router.routes';
 
 const app = express();
 
 app.use(cors());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
-
-app.use(
-  session({
-    secret: "secreto123",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(Router);
+app.use(router);
 
 export default app;
--- api/src/routes/router.routes.ts
+++ api/src/routes/router.routes.ts
@@ -1,48 +1,32 @@
-
-import { Router } from "express";
-import { CrearComprador, iniciarSesion } from "../controllers/user.controller";
+import { Router } from 'express';
+import { crearComprador, iniciarSesion } from '../controllers/user.controller';
+// Make sure that 'iniciarSesion' is exported as a function with the signature (req: Request, res: Response) => void | Promise<void>
 
 import {
-  CrearVendedor,
+  crearVendedor,
   iniciarSesionVendedor,
-} from "../controllers/vendedor.controller";
+} from '../controllers/vendedor.controller';
 
 import {
-  CrearProducto,
-  obtenerProductos,
+  crearProducto,
   editarProducto,
   eliminarProducto,
-} from "../controllers/producto.controller";
-
-import {
-  registrarComprador,
-  registrarVendedor,
-  solicitarRecuperacion,
-  restablecerPassword,
-} from "../controllers/autenticacion.controller";
-
-import passport from "passport";
+  obtenerProductos,
+} from '../controllers/producto.controller';
 
 const router = Router();
 //comprador routes
-router.post("/comprador", CrearComprador);
-router.post("/loginComprador", iniciarSesion);
+router.post('/comprador', crearComprador);
+router.post('/loginComprador', iniciarSesion);
 
 //vendedor routes
-router.post("/vendedor", CrearVendedor);
-router.post("/loginVendedor", iniciarSesionVendedor);
+router.post('/vendedor', crearVendedor);
+router.post('/loginVendedor', iniciarSesionVendedor);
 
 //producto routes
-router.post("/producto", CrearProducto);
-router.get("/productos", obtenerProductos);
-router.put("/producto/:id", editarProducto);
-router.delete("/producto/:id", eliminarProducto);
-
-//passpot local
-
-router.post("/registrar/comprador", registrarComprador);
-router.post("/registrar/vendedor", registrarVendedor);
-router.post("/recuperar", solicitarRecuperacion);
-router.post("/restablecer/:token", restablecerPassword);
+router.post('/productos', crearProducto);
+router.get('/productos', obtenerProductos);
+router.put('/productos/:id', editarProducto);
+router.delete('/productos/:id', eliminarProducto);
 
 export default router;
