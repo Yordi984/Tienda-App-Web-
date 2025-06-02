9e39e453182a5d2c3e425cc6751419651ffe0110
joseeduardohg <101235315+joseeduardohg@users.noreply.github.com>
2025-06-02 00:16:51 -0500

    Add more components

---
+++ client/src/components/HeaderWithSearchbar.tsx
@@ -0,0 +1,49 @@
+import styles from './HeaderWithSearchbar.module.css';
+import ButtonComponent from './ui/ButtonComponent';
+import SearchBar from './ui/SearchBar';
+
+interface HeaderWithSearchbarProps {
+  title: string;
+}
+
+export default function HeaderWithSearchbar({
+  title,
+}: HeaderWithSearchbarProps) {
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__green}>
+        <span className={styles.header__title}>{title}</span>
+      </div>
+
+      <div className={styles.header__other}>
+        <div className={styles.headerCategories}>
+          <span className={styles.headerCategories__categoriesTitle}>
+            Categorías
+          </span>
+
+          <ul className={styles.headerCategories__categoriesList}>
+            <li className={styles.headerCategories__categoryItemActive}>
+              Todo
+            </li>
+            <li className={styles.headerCategories__categoryItem}>Comida</li>
+            <li className={styles.headerCategories__categoryItem}>Ropa</li>
+            <li className={styles.headerCategories__categoryItem}>
+              Accesorios
+            </li>
+            <li className={styles.headerCategories__categoryItem}>Dulces</li>
+          </ul>
+        </div>
+
+        <ButtonComponent
+          text='Vender'
+          color='green'
+        />
+
+        <SearchBar
+          placeholder='Buscar'
+          onSearch={() => {}}
+        />
+      </div>
+    </header>
+  );
+}
