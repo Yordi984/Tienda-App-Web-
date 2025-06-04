3a49d9280bc027b06363d58a6a23afd3551a1a12
joseeduardohg <101235315+joseeduardohg@users.noreply.github.com>
2025-06-04 12:44:44 -0500

    Resolve conclicts

--- api/src/routes/router.routes.ts
+++ api/src/routes/router.routes.ts
@@ -1,71 +1,62 @@
-
-import { Router } from "express";
-import { crearComprador, iniciarSesion } from "../controllers/user.controller";
-import multer from "multer";
+import { Router } from 'express';
+import { crearComprador, iniciarSesion } from '../controllers/user.controller';
 
 import {
   crearVendedor,
-
- 
-} from "../controllers/vendedor.controller";
+  obtenerMisFavoritosVendedor,
+} from '../controllers/vendedor.controller';
 
 import {
   crearProducto,
-  obtenerProductos,
   editarProducto,
   eliminarProducto,
- obtenerProductoPorId,
- obtenerMisProductos
-} from "../controllers/producto.controller";
+  obtenerMisProductos,
+  obtenerProductoPorId,
+  obtenerProductos,
+} from '../controllers/producto.controller';
 
 import {
- 
   registrarVendedor,
-  solicitarRecuperacion,
   restablecerPassword,
-} from "../controllers/autenticacion.controller";
+  solicitarRecuperacion,
+} from '../controllers/autenticacion.controller';
 
-import { Favorito, obtenerMisFavoritos } from "../controllers/favoritos.controller";
+import {
+  Favorito,
+  obtenerMisFavoritos,
+} from '../controllers/favoritos.controller';
 
 import { upload } from '../middlewares/upload'; // ruta según tu estructura
 
-
-
-
-
 const router = Router();
 //login para los 2 tipos de usuarios
-router.post("/login", iniciarSesion);
-
+router.post('/login', iniciarSesion);
 
 //comprador routes
-router.post("/comprador", crearComprador);
-
+router.post('/comprador', crearComprador);
 
 //vendedor routes
-router.post("/vendedor", crearVendedor);
-
-
-
+router.post('/vendedor', crearVendedor);
 
 //producto routes
-router.post("/producto", upload.single('imagen'), crearProducto);
+router.post('/producto', upload.single('imagen'), crearProducto);
 
-router.get("/productos", obtenerProductos);
-router.get("/producto/:id", obtenerProductoPorId);
-router.put("/producto/:id",upload.single('imagen'), editarProducto);
-router.delete("/producto/:id", eliminarProducto);
-router.get("/mis-productos/:vendedorId", obtenerMisProductos);
+router.get('/productos', obtenerProductos);
+router.get('/producto/:id', obtenerProductoPorId);
+router.put('/producto/:id', upload.single('imagen'), editarProducto);
+router.delete('/producto/:id', eliminarProducto);
+router.get('/mis-productos/:vendedorId', obtenerMisProductos);
+router.get('/vendedores/:vendedorId/favoritos', obtenerMisFavoritosVendedor);
 
 //passpot local
 
+router.post('/registrar/vendedor', registrarVendedor);
+router.post('/recuperar', solicitarRecuperacion);
+router.post('/restablecer/:token', restablecerPassword);
 
-router.post("/registrar/vendedor", registrarVendedor);
-router.post("/recuperar", solicitarRecuperacion);
-router.post("/restablecer/:token", restablecerPassword);
+//Favoritos
 
-//Favoritos 
+router.post('/favorito/:productoId', Favorito);
+router.get('/mis-favoritos/:vendedorId', obtenerMisFavoritos);
 
-router.post("/favorito/:productoId", Favorito);
-router.get("/mis-favoritos/:vendedorId", obtenerMisFavoritos);
 export default router;
