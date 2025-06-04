605bf267b05b00a7fa217816a934fe40f6e42c3d
joseeduardohg <101235315+joseeduardohg@users.noreply.github.com>
2025-06-04 13:43:29 -0500

    Use simpler header in Favoritos page

--- client/src/pages/favoritos/Favoritos.tsx
+++ client/src/pages/favoritos/Favoritos.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import HeaderWithSearchbar from '../../components/HeaderWithSearchbar';
 import ProductCard from '../../components/ProductCard';
+import HeaderComponent from '../../components/ui/HeaderComponent';
 import NavBar from '../../components/ui/Navbar';
 import {
   changeFavorite,
@@ -10,26 +10,13 @@ import type { Product } from '../../types';
 import styles from './Favoritos.module.css';
 
 export default function Productos() {
-  const [searchTerm, setSearchTerm] = useState<string | null>(null);
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
   const [products, setProducts] = useState<Product[]>([]);
 
   const vendedorId = localStorage.getItem('vendedorId');
 
-  const categories = [
-    { label: 'Comida', value: 'comida' },
-    { label: 'Ropa', value: 'ropa' },
-    { label: 'Tecnología', value: 'tecnologia' },
-    { label: 'Accesorios', value: 'accesorios' },
-    { label: 'Otros', value: 'otros' },
-  ];
-
   useEffect(() => {
     if (vendedorId) {
-      getFavoriteProducts(parseInt(vendedorId, 10), {
-        searchTerm: searchTerm ?? undefined,
-        category: selectedCategory ?? undefined,
-      })
+      getFavoriteProducts(parseInt(vendedorId, 10))
         .then((data) => {
           setProducts(data);
         })
@@ -38,17 +25,7 @@ export default function Productos() {
           setProducts([]);
         });
     }
-  }, [vendedorId, searchTerm, selectedCategory]);
-
-  const handleSearch = (searchTerm: string) => {
-    setSearchTerm(searchTerm);
-    console.log(`Search term updated: ${searchTerm}`);
-  };
-
-  const handleFilter = (category: string) => {
-    setSelectedCategory(category);
-    console.log(`Category selected: ${category}`);
-  };
+  }, [vendedorId]);
 
   const handleFavorite = (product: Product) => {
     if (vendedorId) {
@@ -60,12 +37,7 @@ export default function Productos() {
 
   return (
     <div>
-      <HeaderWithSearchbar
-        title='Hola, ¿qué comprarás hoy?'
-        categories={categories}
-        onSearch={handleSearch}
-        onFilter={handleFilter}
-      />
+      <HeaderComponent text='Mis favoritos' />
 
       <div className={styles.productGrid}>
         {products.map((product) => (
